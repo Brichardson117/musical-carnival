d94f9768f874ac204603c410dbed09b249865758
Brittney <brittneyrichardson8@gmail.com>
2023-01-10 13:44:56 -0600

    created events object

---
+++ script.js
@@ -0,0 +1,33 @@
+let eventSection = document.getElementById('eventCards')
+
+const events = [
+    {
+        EventName: 'Book Drive',
+        Date: 'February 12, 2023',
+        location: 'to be determined'
+    },
+    {
+        EventName: 'Food Drive',
+        Date:'January 30, 2023',
+        location: 'to be determined'
+    },
+    {
+        EventName: 'Clothes and Shoes drive',
+        Date:'February 20, 2023',
+        location: 'to be determined'
+    }, 
+    {
+        EventName: 'Free tutoring',
+        Date: 'Every Thursday',
+        location: 'To be detemined'
+    }
+]
+
+function upcomingEvent() {
+    for(let i = 0; i < events.length; i++) {
+        let cardDiv = document.createElement('div')
+        let Name = document.createElement('h3')
+    }
+}
+
+upcomingEvent()
