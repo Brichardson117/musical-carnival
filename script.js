2c8c156fafc29e96159bf85bf925c5216fcaa9b1
Brittney <brittneyrichardson8@gmail.com>
2023-01-10 14:45:25 -0600

    mvp for upcomingEvents function done

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const events = [
     {
         EventName: 'Book Drive',
         Date: 'February 12, 2023',
-        location: 'to be determined'
+        location: 'to be determined',
+        
     },
     {
         EventName: 'Food Drive',
@@ -26,7 +27,30 @@ const events = [
 function upcomingEvent() {
     for(let i = 0; i < events.length; i++) {
         let cardDiv = document.createElement('div')
+
+        //event image section
+
+        //event name section
         let Name = document.createElement('h3')
+        Name.textContent = events[i].EventName
+
+        //event date section
+        let eventDate = document.createElement('h4')
+        eventDate.textContent = events[i].Date
+
+        //event location
+        let eventLocation = document.createElement('h5')
+        eventLocation.textContent = events[i].location
+
+        //learn more button
+
+        let learnMoreBtn = document.createElement('button')
+        learnMoreBtn.textContent = 'Learn More'
+
+        cardDiv.append(Name,eventDate,eventLocation,learnMoreBtn)
+        
+
+        eventSection.append(cardDiv)
     }
 }
 
